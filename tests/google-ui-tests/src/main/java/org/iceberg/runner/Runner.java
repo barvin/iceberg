ae83386585f1cf71a11a7b54a626ec26e6e2e1dd
Maksym Barvinskyi <Maksym_Barvinskyi@epam.com>
2017-12-12 15:45:45 +0200

    not really working state, just backup

--- tests/google-ui-tests/src/main/java/org/iceberg/runner/Runner.java
+++ tests/google-ui-tests/src/main/java/org/iceberg/runner/Runner.java
@@ -1,23 +1,58 @@
 package org.iceberg.runner;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.home.config.PropertiesManager;
 import org.home.config.TestType;
 import org.home.reportportal.IcebergTestNGListener;
 import org.home.tests.GoogleTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.ITestNGListener;
 import org.testng.TestNG;
 
 public class Runner {
-    public static void main(String[] args) {
-        new PropertiesManager().initProperties(TestType.WEB);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Runner.class);
 
-        TestNG testNG = new TestNG();
-        boolean isRpEnabled = "true".equals(System.getProperty("rp.enable"));
-        if (isRpEnabled) {
-            testNG.setUseDefaultListeners(false);
-            testNG.addListener((ITestNGListener) new IcebergTestNGListener());
+    public static void main(String[] args) {
+        String role = "leader";
+        for (String arg : args) {
+            if (arg.contains("=") && arg.split("=")[0].equals("role")) {
+                role = arg.split("=")[1];
+            }
         }
-        testNG.setTestClasses(new Class[] { GoogleTest.class });
-        testNG.run();
+        LOGGER.info("ROLE: " + role);
+        if ("leader".equals(role)) {
+            new PropertiesManager().initProperties(TestType.WEB);
+            try {
+                int exitValue = exec(Runner.class, "role=worker");
+                LOGGER.info("exitValue = {}", exitValue);
+            } catch (IOException | InterruptedException e) {
+                LOGGER.error(e.getLocalizedMessage(), e);
+            }
+        } else if ("worker".equals(role)) {
+            TestNG testNG = new TestNG();
+            boolean isRpEnabled = "true".equals(System.getProperty("rp.enable"));
+            if (isRpEnabled) {
+                testNG.setUseDefaultListeners(false);
+                testNG.addListener((ITestNGListener) new IcebergTestNGListener());
+            }
+            testNG.setTestClasses(new Class[] { GoogleTest.class });
+            testNG.run();
+        }
+    }
+
+    public static int exec(Class clazz, String args) throws IOException, InterruptedException {
+        String javaHome = System.getProperty("java.home");
+        String javaBin = javaHome + File.separator + "bin" + File.separator + "java";
+        String classpath = System.getProperty("java.class.path");
+        String className = clazz.getCanonicalName();
+
+        ProcessBuilder builder = new ProcessBuilder(javaBin, "-cp", classpath, className, args);
+
+        Process process = builder.start();
+        process.waitFor();
+        return process.exitValue();
     }
 }
